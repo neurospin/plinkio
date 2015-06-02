940617e2766f78b2365ae7514abcb8f797a490fe
Antoine Grigis <antoine.grigis@cea.fr>
2015-06-02 14:09:39 +0200

    plinkio/info: create stable version 1.0.0.

--- plinkio/info.py
+++ plinkio/info.py
@@ -8,25 +8,23 @@
 ##########################################################################
 
 # Capsul current version
-version_major = 0
-version_minor = 1
-version_micro = 1
-version_extra = ""
+version_major = 1
+version_minor = 0
+version_micro = 0
 
 # The following variables are here for backward compatibility in order to
 # ease a transition for bv_maker users. They will be removed in a few days.
 _version_major = version_major
 _version_minor = version_minor
 _version_micro = version_micro
-_version_extra = version_extra
 
 # Expected by setup.py: string of form "X.Y.Z"
-__version__ = "{0}.{1}.{2}{3}".format(
-    version_major, version_minor, version_micro, version_extra)
+__version__ = "{0}.{1}.{2}".format(
+    version_major, version_minor, version_micro)
 
 # Expected by setup.py: the status of the project
 CLASSIFIERS = [
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
@@ -65,7 +63,7 @@ CLASSIFIERS = CLASSIFIERS
 AUTHOR = "PLINKIO developers"
 AUTHOR_EMAIL = "vincent.frouin@cea.fr"
 PLATFORMS = "OS Independent"
-ISRELEASE = version_extra == ""
+ISRELEASE = True
 VERSION = __version__
 PROVIDES = ["plinkio"]
 REQUIRES = []
